be2858264b91f930f65f9163dde69f7b2f90a655
amineghedira <amine.ghedira@tbs.u-tunis.tn>
2023-08-18 17:55:49 +0100

    continued to test and work on services

--- app/Services/BrawlStarsServices.php
+++ app/Services/BrawlStarsServices.php
@@ -2,29 +2,31 @@
 
 namespace App\Services ;
 
+require 'App/Services/BrawlStarsClient.php' ;
 use App\Services\BrawlStarsClient ;
+
 
 class BrawlStarsServices {
 
-    // Properties 
-    //private static $apitoken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFiODFiZTdlLTQxMTEtNDZhYi04YzZhLWZiYzFlYTI1ZWZhMiIsImlhdCI6MTY5MjE3OTQxMCwic3ViIjoiZGV2ZWxvcGVyLzBmZDBlNDZkLTliZTEtZTQzMC1lODZjLTI2YWRjZDdmYzA5NyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTk3LjI2LjEwMi4xODMiXSwidHlwZSI6ImNsaWVudCJ9XX0.u-x7q40B3KIbfJePVRJkQ8bKToxp9Lj0p64TdcaWFtOCiJWhaLzIvDbztuMNbUD73ChZAvjI050GGFkt45NV-w';
-    private $client ;
 
+
+    // Properties 
+    private $client ;
     // Constructor
-    public function __construct ($apitoken) {
-        $this->client = new BrawlStarsClient($apitoken) ;
+    public function __construct($apitoken) {
+           $this->client = new BrawlStarsClient($apitoken);
     }
 
     // Methods
     public function getBattleLog($tag) {
 
-        $battleLog = $client->getPlayerBattleLog($tag);
+        $battleLog = $this->client->getPlayerBattleLog($tag);
         return $battleLog ;
     }
 
-    public function checkBattleDate($battleLog, $battleIndex) {
+    public function checkBattleDate($battle) {
 
-        $battleTime = $battleLog[$battleIndex]['battleTime'];
+        $battleTime = $battle['battleTime'];
         $battleDate = substr($battleTime,0,8);
         $currentDate = gmdate('Ymd');
     
@@ -35,15 +37,32 @@ class BrawlStarsServices {
          return false;
     }
 
-    public function checkBattle($battleLog, $battleIndex) {
-        
+    public function checkBattle($battle) {
 
+        if (!$this->checkBattleDate($battle))
+          return false;
+
+        $type = $battle['battle']['type'] ;
+        $mode = $battle['battle']['mode'];
+        $map = $battle['event']['map'] ;
+
+        if ($type ==='ranked') {
+
+          if( strpos($mode,'Showdown') || $map === null )
+            return false;
+
+          else
+            return true;  
+        }
+        
+        return false; 
     }
 
-    public function isFirstTeam($teams, $tag){
-        for ($i=0 ; $i<6 ; $i++){
+    public function isFirstTeam($players, $tag) {
 
-            $player = $teams[i];
+        for ($i=0 ; $i<3 ; $i++){
+
+            $player = $players[$i];
     
             if ($player['tag'] === '#'.$tag)
               
@@ -57,62 +76,77 @@ class BrawlStarsServices {
 
         $map = $battle['event']['map'];
         $result = $battle['battle']['result'];
+
         $data = [
             'map' => $map,
             'result' => $result
-        ];
+        ] ;
+
         $brawlers = array_map(fn($player) => $player['brawler']['name'], $players) ;
 
-        if (($isFirstTeam && $result === 'defeat' ) || ($isFirstTeam && $result === 'victory'))
-          
-         return array_merge($data, array_reverse($brawlers));
+        $keys = array_map( fn($i) => 'brawler'.$i, range(1, 6));
         
-        else
+        if (($isFirstTeam && ($result === 'defeat') ) || ($isFirstTeam && ($result === 'victory'))) {
+          
+         $brawlers = array_combine($keys, array_reverse($brawlers)); 
 
          return array_merge($data, $brawlers);
+        }
+        
+        else {
+
+         $brawlers = array_combine($keys, $brawlers);
+
+         return array_merge($data, $brawlers);
+        }
         
     }
 
-    public function getBattleData($battleLog, $index, $tag) {
+    public function getBattleData($battle, $tag) {
 
-    $battle = $battleLog[$index];
-    $players = array_merge(...$battle['battle']['teams']);
+     $players = array_merge(...$battle['battle']['teams']);
 
+     $isFirstTeam = $this->isFirstTeam($players, $tag);
     
-
-    $isFirstTeam = isFirstTeam($players, $tag);
-    
-    return battleData($battle, $players, $isFirstTeam);
+     return $this->battleData($battle, $players, $isFirstTeam) ;
 
     }
     
 
     public function checkBattleLog($tag) {
-
-     $Data = [];
-     $battleLog = getBattleLog($tag)
-
-     //check whether the latest Battle is of today's date.
-     if (checkBattle($battleLog, 0)){
-        
-        $Data["tag"] = $tag;
-        $Data["battle1"]=getBattleInfo($battleLog, 0, $tag) ;
-
-        for($i=1; i<25; $i++){
-        
-          if(checkBattle($battleLog, $i))
-            
-
-          
-
-
-      }
-    
-
      
-  }
+     $data =[];
+     $battleLog = $this->getBattleLog($tag);
+     $battle = $battleLog[0];
+
+     //check whether the latest Battle is of today's date and a ranked mode.
+     if ($this->checkBattle($battle)) {
+
+        $data['tag'] = $tag ;
+        $data['battle1'] = $this->getBattleData($battle, $tag) ;
+
+        for($i=1; $i<25; $i++){
+
+          $battle = $battleLog[$i];
+
+          if($this->checkBattle($battle))
+
+            $data['battle'.($i+1)] = $this->getBattleData($battle, $tag);
+
+          else
+          
+            return $data;                
+        }
+
+        return $data;
+        }
+ 
+     return false;
+    }
+
  
 
-}
+
+
 
 }
